80c5b56fd4958b4ee5410586979a547b3c4d3ea0
furantuturu <fuwansuu@gmail.com>
2025-03-12 18:25:36 +0800

    new routes added

--- routes.php
+++ routes.php
@@ -1,3 +1,12 @@
 <?php
 
-$router->get('/', 'dashboard.php');
+$router->get('/dashboard', 'dashboard.php');
+
+//Comunication related routes
+$router->get('/communications', 'communications/index.php');
+$router->get('/communications/create', 'communications/create.php');
+
+$router->get('/reports', 'reports/index.php');
+
+$router->get('/attach', 'attach/index.php');
+
