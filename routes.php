1af83f07d61cff3fdc0aaf21f158f8225dd3d501
furantuturu <fuwansuu@gmail.com>
2025-03-13 22:02:05 +0800

    more routes

--- routes.php
+++ routes.php
@@ -4,9 +4,14 @@ $router->get('/dashboard', 'dashboard.php');
 
 //Comunication related routes
 $router->get('/communications', 'communications/index.php');
-$router->get('/communications/create', 'communications/create.php');
+$router->get('/communications-create', 'communications/create.php');
+$router->post('/communications-create', 'communications/store.php');
 
+//Reports related routes
 $router->get('/reports', 'reports/index.php');
 
+//File attachment related routes
 $router->get('/attach', 'attach/index.php');
+$router->post('/attach', 'attach/upload.php');
+$router->delete('/attach', 'attach/delete.php');
 
