c3fbd7c720bfb81222a2fa0d0e354cdc1acba386
furantuturu <fuwansuu@gmail.com>
2025-03-13 22:05:17 +0800

    updated index controller for /attach route

--- controllers/attach/index.php
+++ controllers/attach/index.php
@@ -1,5 +1,24 @@
 <?php
 
+use Classes\Session;
+
+$db = getDatabaseClass();
+$uploadedPDFS = $db->query("SELECT * FROM uploads ORDER BY created_at DESC")->getAll();
+
+$uploadErr = "";
+if (Session::get('fileexists')) {
+    $uploadErr = Session::get('fileexists'); 
+} else if (Session::get('filetypeerr')) {
+    $uploadErr = Session::get('filetypeerr');
+} else if ( Session::get('fileerror')) {
+    $uploadErr =  Session::get('fileerror');
+}
+
 return view('attach.view.php', [
-    'title' => "Upload PDF"
+    'hasTrackerClass' => "",
+    'title' => "Upload PDF",
+    'uploadErr' => $uploadErr,
+    'uploadSucc' => Session::get('filesuccess'),
+    'fileDel' => Session::get('filedelete'),
+    'uploadedPDFS' => $uploadedPDFS
 ]);
