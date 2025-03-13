a6cee22396c97eaf6edad7dd2e653688117933d5
furantuturu <fuwansuu@gmail.com>
2025-03-13 22:05:38 +0800

    handle uploading of pdf files from /attach route

---
+++ controllers/attach/upload.php
@@ -0,0 +1,28 @@
+<?php
+
+use Classes\Session;
+use Classes\ValidateFile;
+
+$targetDir = "uploads/";
+$targetPdfFile = basename($_FILES['attachment']['name']);
+$uploadDir = $targetDir . $targetPdfFile;
+$pdfFileType = strtolower(pathinfo($targetPdfFile, PATHINFO_EXTENSION));
+
+if (ValidateFile::validate($pdfFileType, $uploadDir)) {
+    redirect('/attach');
+}
+
+if (move_uploaded_file($_FILES['attachment']['tmp_name'], $uploadDir)) {
+    $db = getDatabaseClass();
+
+    $db->query("INSERT INTO uploads (filename) VALUES (:pdf)", [
+        ':pdf' => $targetPdfFile
+    ]);
+
+    Session::flash('filesuccess','PDF File has been uploaded');
+} else {
+    Session::flash('fileerror','PDF file can\'t be uploaded');
+}
+
+redirect('/attach');
+
