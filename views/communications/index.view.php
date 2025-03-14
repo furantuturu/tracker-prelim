e6308e79b34a132681f4bb3696f0e7f21580857e
furantuturu <fuwansuu@gmail.com>
2025-03-14 23:22:53 +0800

    rendering retrieved data from db

--- views/communications/index.view.php
+++ views/communications/index.view.php
@@ -5,11 +5,12 @@
     <h1>Communications List 📰</h1>
     <fieldset class="communication-actions">
         <a href="/communications-create" role="button" tabindex="0">Add New</a>
-        <form action="" method="post" role="search">
+        <form action="/communications" method="post" role="search">
             <input type="search" name="search-communication" placeholder="Search by Barcode, Sender, or Subject">
             <input type="submit" value="Search">
         </form>
     </fieldset>
+    <small>*If you search by barcode, search it by its number.</small>
     <div class="overflow-auto">
         <table class="striped">
             <thead>
@@ -22,14 +23,55 @@
                     <th scope="col">Action Required</th>
                     <th scope="col">Status</th>
                     <th scope="col">Target Date</th>
+                    <th scope="col">Days Overdue</th>
                     <th scope="col">Date Received</th>
                     <th scope="col">PDF / File Attachment</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                <?php foreach($communications as $com): ?>
+                    <tr>
+                        <th scope="row">GFA-<?= $com['communication_id'] ?></th>
+                        <td><?= $com['sender'] ?></td>
+                        <td><?= $com['subject'] ?></td>
+                        <td><?= $com['docdate'] ?></td>
+                        <td><?= $com['category'] ?></td>
+                        <td><?= $com['action_required'] ?></td>
+                        <td><?= $com['status'] ?></td>
+                        <td><?= $com['target_date'] ?></td>
+                        <td><?= $com['overdue'] ?></td>
+                        <td><?= $com['date_received'] ?></td>
+                        <td>
+                            <?php if (empty($com['pdf_file'])): ?>
+                                No Attachment
+                            <?php else: ?>
+                                <a href="communication-uploads/<?= $com['pdf_file'] ?>" target="_blank" class="com-view-btn">
+                                    View Attachment
+                                </a>
+                            <?php endif; ?>
+                        </td>
+                        <td class="com-actions">
+                            <a href="/communications-edit?id=<?= $com['communication_id'] ?>" class="com-edit-btn">Edit</a>
+                            <form action="/communications" method="post">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="_id" value="<?= $com['communication_id'] ?>">
+                                <input type="hidden" name="_file" value="communication-uploads/<?= $com['pdf_file'] ?>">
+                                <button type="submit" class="com-del-btn">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
     </div>
+    <?php if ($pageLen >= 1): ?>
+        <div class="pagination">
+            <?php for ($i=0; $i <= $pageLen; $i++): ?>
+                <a href="/communications?p=<?= $i + 1; ?>"><?= $i + 1; ?></a>
+            <?php endfor; ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php require ROOT . 'views/partials/footer.partial.php' ?>
