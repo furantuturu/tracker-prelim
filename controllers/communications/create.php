70d9c8b1b5590cf2924ca97322f22f45d1c04f0a
furantuturu <fuwansuu@gmail.com>
2025-03-14 23:17:43 +0800

    added more var for flash messages

--- controllers/communications/create.php
+++ controllers/communications/create.php
@@ -1,6 +1,21 @@
 <?php
 
+use Classes\Session;
+
+$fileErr = "";
+if (Session::get('fileexists')) {
+    $fileErr = Session::get('fileexists'); 
+} else if (Session::get('filetypeerr')) {
+    $fileErr = Session::get('filetypeerr');
+} else if (Session::get('fileerror')) {
+    $fileErr =  Session::get('fileerror');
+}
+
 return view('communications/create.view.php', [
     'hasTrackerClass' => "",
-    'title' => "Create Communication"
+    'title' => "Create Communication",
+    "emptyField" => Session::get("emptyfielderror"),
+    "dayErr" => Session::get("dayerror"),
+    "uploadErr" => Session::get("uploaderror"),
+    "fileErr" => $fileErr
 ]);
