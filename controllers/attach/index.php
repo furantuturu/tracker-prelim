2954c42aae1d2d7eae4cf0e06a555eaf54b251b6
furantuturu <fuwansuu@gmail.com>
2025-03-14 23:16:28 +0800

    change var name for more fitting

--- controllers/attach/index.php
+++ controllers/attach/index.php
@@ -5,20 +5,20 @@ use Classes\Session;
 $db = getDatabaseClass();
 $uploadedPDFS = $db->query("SELECT * FROM uploads ORDER BY created_at DESC")->getAll();
 
-$uploadErr = "";
+$fileErr = "";
 if (Session::get('fileexists')) {
-    $uploadErr = Session::get('fileexists'); 
+    $fileErr = Session::get('fileexists'); 
 } else if (Session::get('filetypeerr')) {
-    $uploadErr = Session::get('filetypeerr');
+    $fileErr = Session::get('filetypeerr');
 } else if ( Session::get('fileerror')) {
-    $uploadErr =  Session::get('fileerror');
+    $fileErr =  Session::get('fileerror');
 }
 
 return view('attach.view.php', [
     'hasTrackerClass' => "",
     'title' => "Upload PDF",
-    'uploadErr' => $uploadErr,
-    'uploadSucc' => Session::get('filesuccess'),
+    'fileErr' => $fileErr,
+    'fileSucc' => Session::get('filesuccess'),
     'fileDel' => Session::get('filedelete'),
     'uploadedPDFS' => $uploadedPDFS
 ]);
