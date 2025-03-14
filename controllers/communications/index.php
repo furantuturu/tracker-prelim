827ae1d9192906bdb280f4c0b9063e6f01c95dca
furantuturu <fuwansuu@gmail.com>
2025-03-14 23:19:44 +0800

    added capabilities of search param and pagination for retrieving data

--- controllers/communications/index.php
+++ controllers/communications/index.php
@@ -1,6 +1,23 @@
 <?php
 
+$db = getDatabaseClass();
+
+$communications = "";
+$offset = (empty($_GET['p']) ? 0 : $_GET['p'] - 1);
+
+if (empty($_GET['q'])) {
+    $communications = $db->query("SELECT * FROM communications ORDER BY created_at DESC LIMIT 5 OFFSET {$offset}")->getAll();
+} else {
+    $searchVal = $_GET['q'];
+    $communications = $db->query("SELECT * FROM communications WHERE communication_id = :id OR sender LIKE :q OR subject LIKE :q ORDER BY created_at DESC LIMIT 5 OFFSET {$offset}", [
+        ':id' => $searchVal,
+        ':q' => "%{$searchVal}%",
+    ])->getAll();
+}
+
 return view('communications/index.view.php', [
     'hasTrackerClass' => "tracker-main",
-    'title' => "Communications Dashboard"
+    'title' => "Communications Dashboard",
+    'communications' => $communications,
+    'pageLen' => floor(count($communications) / 5)
 ]);
