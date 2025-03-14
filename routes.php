59ef2cd5486378ce5872e867ebe8f8b8955db156
furantuturu <fuwansuu@gmail.com>
2025-03-14 23:15:28 +0800

    added routes

--- routes.php
+++ routes.php
@@ -1,11 +1,14 @@
 <?php
-
 $router->get('/dashboard', 'dashboard.php');
 
 //Comunication related routes
 $router->get('/communications', 'communications/index.php');
 $router->get('/communications-create', 'communications/create.php');
 $router->post('/communications-create', 'communications/store.php');
+$router->get('/communications-edit', 'communications/edit.php');
+$router->put('/communications-edit', 'communications/update.php');
+$router->delete('/communications', 'communications/delete.php');
+$router->post('/communications', 'communications/search.php');
 
 //Reports related routes
 $router->get('/reports', 'reports/index.php');
