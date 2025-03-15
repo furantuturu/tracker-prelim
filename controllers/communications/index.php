f11c20e6f859d79952f0ea581eb7e6d25ee749f9
furantuturu <fuwansuu@gmail.com>
2025-03-15 16:03:27 +0800

    added another db query for getting total cound of  communication data

--- controllers/communications/index.php
+++ controllers/communications/index.php
@@ -2,14 +2,18 @@
 
 $db = getDatabaseClass();
 
-$communications = "";
-$offset = (empty($_GET['p']) ? 0 : $_GET['p'] - 1);
+$offset = empty($_GET['p']) ? 0 : ($_GET['p'] - 1) * 5;
 
 if (empty($_GET['q'])) {
     $communications = $db->query("SELECT * FROM communications ORDER BY created_at DESC LIMIT 5 OFFSET {$offset}")->getAll();
+    $communicationsLen = $db->query('SELECT count(communication_id) FROM communications')->getAll();
 } else {
     $searchVal = $_GET['q'];
     $communications = $db->query("SELECT * FROM communications WHERE communication_id = :id OR sender LIKE :q OR subject LIKE :q ORDER BY created_at DESC LIMIT 5 OFFSET {$offset}", [
+        ':id' => $searchVal,
+        ':q' => "%{$searchVal}%",
+    ])->getAll();
+    $communicationsLen =$db->query("SELECT count(communication_id) FROM communications WHERE communication_id = :id OR sender LIKE :q OR subject LIKE :q", [
         ':id' => $searchVal,
         ':q' => "%{$searchVal}%",
     ])->getAll();
@@ -19,5 +23,6 @@ return view('communications/index.view.php', [
     'hasTrackerClass' => "tracker-main",
     'title' => "Communications Dashboard",
     'communications' => $communications,
-    'pageLen' => floor(count($communications) / 5)
+    'pageLen' => ceil($communicationsLen[0]["count(communication_id)"] / 5),
+    'query' => $_GET['q'] ?? ''
 ]);
