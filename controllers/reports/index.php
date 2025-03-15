424fbe4980952d78ef7c642a33b7622c3a2ec0dd
furantuturu <fuwansuu@gmail.com>
2025-03-15 16:06:03 +0800

    added processing of retrieving data whether it is filtered or not

--- controllers/reports/index.php
+++ controllers/reports/index.php
@@ -1,6 +1,23 @@
 <?php
 
+$db = getDatabaseClass();
+
+if (empty($_GET['s']) && empty($_GET['d'])) {
+    $communicationReports = $db->query("SELECT communication_id, sender, subject, docdate, status FROM communications ORDER BY created_at DESC")->getAll();
+} else {
+    $status = $_GET['s'];
+    $docdate = $_GET['d'];
+    $filtered = true;
+
+    $communicationReports = $db->query("SELECT communication_id, sender, subject, docdate, status FROM communications WHERE status = :status AND docdate = :docdate ORDER BY created_at DESC", [
+        ':status' => $status,
+        ':docdate' => $docdate
+    ])->getAll();
+}
+
 return view('reports.view.php', [
     'hasTrackerClass' => "tracker-main",
-    'title' => "Reports Dashboard"
+    'title' => "Reports Dashboard",
+    'reports' => $communicationReports,
+    'filtered' => $filtered ?? false
 ]);
